e089af3b4cf44365aa92235edb2a5f16c675f122
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2025-01-17 12:16:24 +0000

    updated  Product Response  schema

--- app/project/schemas.py
+++ app/project/schemas.py
@@ -1,7 +1,7 @@
 
 from app.core.schema import BaseSchema
 from pydantic import BaseModel
-from datetime import date
+from datetime import date, datetime
 from typing import Optional, List
 from pydantic import Field
 from app.accounts.schemas import UserResponseSchema
@@ -23,7 +23,7 @@ class ProjectResponseSchema(BaseSchema, BaseModel):
     title: str
     description: Optional[str] = ""
     goal_amount: float = Field(..., gt=0)
-    deadline: date
+    deadline: datetime
     total_contribution: Optional[Decimal] = 0
     contributors: List[UserResponseSchema] = []
 
